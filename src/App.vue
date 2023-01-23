9501c53ba0c1b706b2009c20c672d63471d35f58
rifaldo <rivaldiwanda37@gmail.com>
2023-01-23 19:25:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>helllo {{ datasensor }} {{ datasensor2 }}
+  <button @click="getData" class="bg-orange-500 p-2 rounded-xl">Ambil Data</button> &nbsp;
+  <button @click="getData" class="bg-orange-500 p-2 rounded-xl">Ambil Data satunya</button>
+  </div>
+</template>
+
+<script> //tempat ngoding java script
+export default {
+  data(){
+    return{
+      datasensor : "90",
+      datasensor2 : "80"
+    }
+  },
+  created(){
+    //this.getData()
+   // this.getData1()
+  },
+  methods: {
+    //getData1(){
+    //  alert('hello')
+   // },
+    async getData(){
+      const data = await fetch("http://localhost/sensor1")
+      const datajson = await data.json()
+      console.log(datajson)
+      this.datasensor = datajson.sensor1
+    
+      
+      }
+
+  },
+
+}
+</script>
+
+<style>
+
+</style>
